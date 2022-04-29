1ad2db8e00bae2dea547b5737cd96386108d2bfc
Rodney Sostras <rodney.sostras@gmail.com>
2022-04-28 22:08:03 -0300

    DOC: insert documentation

--- backend/appconfig/core.py
+++ backend/appconfig/core.py
@@ -1,5 +1,8 @@
 # Copyright 2022 the author Rodney Sostras. All rights reserved.
 
+## Core da aplicação
+## Possui a configuração necessaria para iniciar o serviço
+
 from fastapi import FastAPI
 
 from appconfig.router import app_router
